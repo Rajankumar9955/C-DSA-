c8a7bf679f09134abd50e7f3dab5f3bfac7e389e
Rajan <krajan92946@gmail.com>
2024-10-15 17:52:46 +0530

    quick sort theory done

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,14 +1,23 @@
 /* first let first element as a pivot element 
    and lets two point like p and q 
    p is stay in one step away from pivot element
-   if p is greate than  ans equal pivote element then stop
+   if p is greater than  ans equal pivote element then stop
    if q is less than and equal pivote element then stop 
    ans then if doesnot cross the each element then  exchange the position or p and q
    and then p and q cross to each other then q exchange with pivote element
+
+
+   key:- (there are two pointer like p,q and pv means pivote elements)
+   (1)if p>=pv :- stop
+   (2)if q<p :-   stop
+   (3)if (p<q) swap between (p,g)
+   (4)if p cross q then swap(p,pv)
+
 */
 
 #include<iostream>
 using namespace std;
+
 int main()
 {
     
