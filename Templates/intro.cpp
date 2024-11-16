ef7fc447240ba6140594a7982da24eb3ea5af2d3
Rajan <krajan92946@gmail.com>
2024-11-16 18:31:35 +0530

    template in cpp

---
+++ Templates/intro.cpp
@@ -0,0 +1,38 @@
+/*#templates:- it is a generalized format OF FUNCTION & CLASS 
+There are two types of templates
+1. Function Templates
+2. Class Templates
+
+#template:- Content palce holder */
+
+
+
+
+#include<iostream>
+using namespace std;
+
+
+template<typename a, typename b>
+a grt(a a1, b b1){
+    if(a1>b1)
+    {
+        return a1;
+    }
+    else
+    {
+        return b1;
+    }
+}
+template<template a, template b, template c>
+b srt(b a1, b b1, b c1)
+{
+    return a1+b1+c1;
+}
+int main()
+{
+    cout<<"integer comparing"<<grt(200,54);
+    cout<<"float comparing"<<grt(2.0,5.4);
+    cout<<"char comparing"<<grt('A','Z');
+
+    cout<<"addition in template"<<srt(10,10,10);
+}
