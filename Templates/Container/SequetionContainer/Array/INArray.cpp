68342468057f8ae86a656e66e64ce01bec805b2f
Rajan <krajan92946@gmail.com>
2024-11-20 18:02:30 +0530

    Dynamic Arrays

--- Templates/Container/SequetionContainer/Array/INArray.cpp
+++ Templates/Container/SequetionContainer/Array/INArray.cpp
@@ -3,3 +3,17 @@
 #include<iostream>
 using namespace std;
 #include<array>
+int main()
+{
+    array<int,5>arr;
+    cout<<"Enter 5 values\n";
+    for(int i=0; i<arr.size; i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"output is\n";
+    for(int i=0; i<arr.size; i++)
+    {
+        cout<<arr[i]<<"\t";
+    }
+}
