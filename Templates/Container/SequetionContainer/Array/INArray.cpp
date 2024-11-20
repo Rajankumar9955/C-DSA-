351c2f60d4ca2be5ca9990a0b981b434bc63e3e4
Rajan <krajan92946@gmail.com>
2024-11-20 18:13:51 +0530

    front inbuild function uses

--- Templates/Container/SequetionContainer/Array/INArray.cpp
+++ Templates/Container/SequetionContainer/Array/INArray.cpp
@@ -7,13 +7,16 @@ int main()
 {
     array<int,5>arr;
     cout<<"Enter 5 values\n";
-    for(int i=0; i<arr.size; i++)
+    for(int i=0; i<arr.size(); i++)
     {
         cin>>arr[i];
     }
     cout<<"output is\n";
-    for(int i=0; i<arr.size; i++)
+    for(int i=0; i<arr.size(); i++)
     {
         cout<<arr[i]<<"\t";
     }
+    cout<<"\n";
+    cout<<"index of 0 indexing:"<<arr.front()<<"\n"; //first data return in array
+    cout<<"Value at n-1 indexing:"<<arr.back()<<"\n"; //last Data return in array
 }
