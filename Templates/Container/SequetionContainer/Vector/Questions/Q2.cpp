0e47983c5c3ceaf21842f999b14065a3625fd070
Rajan <krajan92946@gmail.com>
2024-11-25 18:31:45 +0530

    question 2in vector

--- Templates/Container/SequetionContainer/Vector/Questions/Q2.cpp
+++ Templates/Container/SequetionContainer/Vector/Questions/Q2.cpp
@@ -7,6 +7,10 @@ Ex2:-//input A[0,1,5,4,3,2];   output:- True;
 question2
 WAP to change the first character of each string of a sentense into uppercase
 ex1:- input A ="how much quantity"   output:- How Much Quantity
+
+WAP to return true if second string found in a first string
+input:- s1:-"HEMANT"  s2:"HE"   output:-true
+input:- s1:-"honey"  s2:"ne"    output:-false
 */
 
 #include<iostream>
