25a79e0a244f1c2da2ee11c7da0879da7fa96d5d
Rajan <krajan92946@gmail.com>
2024-11-26 18:25:08 +0530

    Q3.Cpp

--- Templates/Container/SequetionContainer/Vector/Questions/Q3.cpp
+++ Templates/Container/SequetionContainer/Vector/Questions/Q3.cpp
@@ -1,4 +1,31 @@
 
+/*
+question3
+WAP to change the first character of each string of a sentense into uppercase
+ex1:- input A ="how much quantity"   output:- How Much Quantity
+*/
 
+#include<iostream>
+#include<string>
 
+using namespace std;
 
+void StringCap(string s)
+{
+    s[0]=toupper(s[0]);
+    for(int i=1; i<s.size(); i++)
+    {
+        if(s[i]==' ')
+        {
+            s[i+1]=toupper(s.size(i+1));
+        }
+    }
+    cout<<s<<"\n";
+}
+
+int main()
+{
+    string s="how much quantity you have";
+    StringCap(s);
+
+}
