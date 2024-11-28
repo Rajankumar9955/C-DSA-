ef408955e06f67c4c71c932edf2efbee45afd7dd
Rajan <krajan92946@gmail.com>
2024-11-28 17:49:24 +0530

    string start

---
+++ Templates/Container/SequetionContainer/String/str.cpp
@@ -0,0 +1,13 @@
+
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string c;
+    cout<<"memory of string="<<sizeof(c)<<"\n";
+    cout<<"enter ur name:\n";
+    cin>>c;
+    cout<<c<<"\n";
+}
