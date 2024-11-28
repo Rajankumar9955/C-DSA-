0b0249a97595ef9dac7f7f9713a35d71cdf549d3
Rajan <krajan92946@gmail.com>
2024-11-28 18:06:16 +0530

    pair intro

---
+++ Templates/Container/SequetionContainer/Pair/pair.cpp
@@ -0,0 +1,8 @@
+
+
+/*
+pair is a class template which is having two parameter and we can insert the value with the help of 
+make_pair function (This is inbuild function)
+we can access the first element by using first keyword along with object name and second element will be
+access by second keyword along with object
+*/
