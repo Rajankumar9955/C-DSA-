acbc80e633818fe89d2c9e4adad31e25cec9ef8b
Rajan <krajan92946@gmail.com>
2024-11-28 18:25:43 +0530

    pair almost done

--- Templates/Container/SequetionContainer/Pair/pair.cpp
+++ Templates/Container/SequetionContainer/Pair/pair.cpp
@@ -12,9 +12,42 @@ access by second keyword along with object
 using namespace std;
 int main()
 {
-    pair<int,string>p {110, "Rajan"};
-    // p=make_pair(110, "Rajan");
-    cout<<p.first<<"\t";
-    cout<<p.second;
+
+        vector<pair<int,string>>v;
+        int t;
+        int rollno;
+        string name;
+        cout<<"Enter how many records\n";
+        cin>>t;
+        for(int i=1; i<=t; i++)
+        {
+            cout<<"Enter rollno\n";
+            cin>>rollno;
+            cout<<"Enter name\n";
+            cin>>name;
+            v.push_back(make_pair(rollno, name));
+        }
+        for(int i=0; i<v.size(); i++)
+        {
+            cout<<v[i].first<<"\t"<<v[i].second<<"\n";
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // pair<int,string>p {110, "Rajan"};
+    // // p=make_pair(110, "Rajan");
+    // cout<<p.first<<"\t";
+    // cout<<p.second;
 
 }
