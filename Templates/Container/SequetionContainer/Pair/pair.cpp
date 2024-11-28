600ec912e9d5b9b8a6ffbdb336a48f08ddeaf1a7
Rajan <krajan92946@gmail.com>
2024-11-28 18:07:17 +0530

    example of pair

--- Templates/Container/SequetionContainer/Pair/pair.cpp
+++ Templates/Container/SequetionContainer/Pair/pair.cpp
@@ -6,3 +6,15 @@ make_pair function (This is inbuild function)
 we can access the first element by using first keyword along with object name and second element will be
 access by second keyword along with object
 */
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    pair<int,string>p;
+    p=make_pair(110, "Rajan");
+    cout<<p.first<<"\t";
+    cout<<p.second;
+
+}
