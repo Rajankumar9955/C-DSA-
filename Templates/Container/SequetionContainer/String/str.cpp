f36b7ad58a6cf43f2f851040e388b24c7d5e15b6
Rajan <krajan92946@gmail.com>
2024-11-28 17:57:21 +0530

    first add the data and last delete the data

--- Templates/Container/SequetionContainer/String/str.cpp
+++ Templates/Container/SequetionContainer/String/str.cpp
@@ -9,9 +9,9 @@ int main()
     cout<<"memory of string="<<sizeof(c)<<"\n";
     cout<<"enter ur name:\n";
     getline(cin,c);
-    cout<<c<<"\n";
+    cout<<"Your name:"<<c<<"\n";
     c.push_back('k');
     cout<<"String="<<c<<"\n";
-    c.pop_pop();
+    c.pop_back();
     cout<<"now string="<<c;
 }
